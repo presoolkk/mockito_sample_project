06edbf88111b9a176b570573e7019bbb29274f06
Akhil KK <Akhilkk@peoplytics-machine12.local.hirrr.com>
2017-05-26 17:08:56 +0530

    initial commit

---
+++ src/main/java/com/learn/mockito/mockito_example/App.java
@@ -0,0 +1,13 @@
+package com.learn.mockito.mockito_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
