f97234098a964cbe86f2cda5ee4029ead889c87e
Akhil KK <presoolkk@gmail.com>
2017-06-16 20:38:51 +0530

    added runner class for junit test suite run

---
+++ src/test/java/com/learn/mockito/runner/Testrunner.java
@@ -0,0 +1,21 @@
+package com.learn.mockito.runner;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import com.learn.mockito.suite.AllTests;
+
+public class Testrunner {
+
+	 public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(AllTests.class);
+
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful());
+	   }
+
+}
